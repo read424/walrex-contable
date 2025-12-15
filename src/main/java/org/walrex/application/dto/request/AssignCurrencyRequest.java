9f4deaa679e758c1abed12e9dda28ee70647f09d
Ronald E. Aybar D <read424@gmail.com>
2025-12-14 21:41:57 -0500

    fix: Completar DTOs de CountryCurrency para serialización JSON
    - Agregar campos a CountryCurrencyResponse (id, countryId, currencyId, isPrimary, isOperational, effectiveDate, createdAt, currencyCode, currencyName)
    - Agregar campos a AssignCurrencyRequest (currencyId, isPrimary, isOperational, effectiveDate)
    - Agregar anotaciones Lombok (@Data, @Builder, @NoArgsConstructor, @AllArgsConstructor)
    
    Soluciona error de serialización: No serializer found for class CountryCurrencyResponse

--- src/main/java/org/walrex/application/dto/request/AssignCurrencyRequest.java
+++ src/main/java/org/walrex/application/dto/request/AssignCurrencyRequest.java
@@ -1,4 +1,20 @@
 package org.walrex.application.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AssignCurrencyRequest {
+
+    private Integer currencyId;
+    private Boolean isPrimary;
+    private Boolean isOperational;
+    private LocalDate effectiveDate;
 }
