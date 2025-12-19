06711058778255dfbfe5666aa8df2deb2d7d0a90
Ronald E. Aybar D <read424@gmail.com>
2025-12-19 18:13:31 -0500

    application/dto: añadir CreateDistritoRequest para creación de distrito vía API

--- src/main/java/org/walrex/application/dto/request/CreateDistritoRequest.java
+++ src/main/java/org/walrex/application/dto/request/CreateDistritoRequest.java
@@ -1,4 +1,26 @@
 package org.walrex.application.dto.request;
 
-public record CreateDistritoRequest() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
+
+public record CreateDistritoRequest(
+        @NotNull(message = "Es obligatorio el atributo idProvince")
+        @NotBlank(message = "El atributo idProvince no puede ser 0")
+        Integer idProvince,
+
+        @NotNull(message = "Es obligatorio el atributo codigo de distrito")
+        @NotEmpty(message = "Codigo no puede estar vacio")
+        @Length(min = 6, max = 6, message = "Codigo debe contener 6 caracteres")
+        @Pattern(regexp = "^[0-9]{6}$", message = "Codigo solo debe contener carácteres numéricos")
+        String code,
+
+        @NotNull(message = "Es obligatorio el atributo nombre del distrito")
+        @NotEmpty(message = "El atributo nombre no puede estar vacio")
+        @Length(min = 3, max = 100, message = "El atributo nombre debe contener entre 3 y 100 caracteres")
+        String nombre
+
+) {
 }
