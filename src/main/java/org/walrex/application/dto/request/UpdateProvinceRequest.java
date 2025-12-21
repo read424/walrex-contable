4b1b90e237accc2aa641b60fde335a024887922a
Ronald E. Aybar D <read424@gmail.com>
2025-12-21 08:56:20 -0500

    feat(application/dto): add Province request DTOs
    - CreateProvinceRequest: Defines contract for creating new provinces
    - UpdateProvinceRequest: Defines contract for updating provinces
    - Includes validation constraints (NotBlank, NotNull, Size)
    - Ensures API input validation and type safety

---
+++ src/main/java/org/walrex/application/dto/request/UpdateProvinceRequest.java
@@ -0,0 +1,21 @@
+package org.walrex.application.dto.request;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
+
+public record UpdateProvinceRequest(
+
+        @NotNull(message = "Es obligatorio el atributo codigo de provincia")
+        @NotEmpty(message = "Codigo no puede estar vacio")
+        @Length(min = 4, max = 4, message = "Codigo debe contener 4 caracteres")
+        @Pattern(regexp = "^[0-9]{4}$", message = "Codigo solo debe contener carácteres numéricos")
+        String code,
+
+        @NotNull(message = "Es obligatorio el atributo nombre de la provincia")
+        @NotEmpty(message = "El atributo nombre no puede estar vacio")
+        @Length(min = 3, max = 100, message = "El atributo nombre debe contener entre 3 y 100 caracteres")
+        String nombre
+) {
+}
