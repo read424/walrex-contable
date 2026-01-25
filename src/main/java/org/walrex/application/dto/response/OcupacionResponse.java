39b0f75d18f32a960f15280ffe743e58bb758b9c
Ronald E. Aybar D <read424@gmail.com>
2026-01-24 19:22:14 -0500

    feat(ocupacion): Create OcupacionResponse DTO
    Introduces , a Data Transfer Object used for returning  entity data.
    
    - Includes uid=1000(developer01) gid=1000(developer01) grupos=1000(developer01),20(dialout),24(cdrom),25(floppy),29(audio),30(dip),44(video),46(plugdev),100(users),106(netdev),112(bluetooth),114(lpadmin),117(scanner),994(ollama),995(docker), , , , , and  fields for a complete representation of an occupation.
    - Utilizes Lombok for boilerplate code generation.

---
+++ src/main/java/org/walrex/application/dto/response/OcupacionResponse.java
@@ -0,0 +1,24 @@
+package org.walrex.application.dto.response;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OcupacionResponse {
+
+    private Long id;
+    private String codigo;
+    private String nombre;
+    private Integer status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
