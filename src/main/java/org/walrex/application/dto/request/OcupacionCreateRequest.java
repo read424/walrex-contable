82c703f91088baf0d755326cd37e55bd8a0497d0
Ronald E. Aybar D <read424@gmail.com>
2026-01-24 19:19:31 -0500

    feat(ocupacion): Create OcupacionCreateRequest DTO
    Introduces , a Data Transfer Object used for creating new  entities.
    
    - Defines fields  and  for the occupation.
    - Includes Jakarta Validation annotations (, ) for basic input validation.
    - Utilizes Lombok for boilerplate code generation.

---
+++ src/main/java/org/walrex/application/dto/request/OcupacionCreateRequest.java
@@ -0,0 +1,26 @@
+package org.walrex.application.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OcupacionCreateRequest {
+
+    @NotBlank(message = "El código de la ocupación no puede estar vacío.")
+    @Size(max = 5, message = "El código de la ocupación no puede exceder los 5 caracteres.")
+    private String codigo;
+
+    @NotBlank(message = "El nombre de la ocupación no puede estar vacío.")
+    @Size(max = 100, message = "El nombre de la ocupación no puede exceder los 100 caracteres.")
+    private String nombre;
+
+}
