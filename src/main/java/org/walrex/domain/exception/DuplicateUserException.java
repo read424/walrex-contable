874e0122cc4168b82ddc2376f2af26be9ecdef53
Ronald E. Aybar D <read424@gmail.com>
2026-01-26 07:27:23 -0500

    Añadido src/main/java/org/walrex/domain/exception/DuplicateUserException.java: 12 líneas

---
+++ src/main/java/org/walrex/domain/exception/DuplicateUserException.java
@@ -0,0 +1,12 @@
+package org.walrex.domain.exception;
+
+public class DuplicateUserException extends RuntimeException {
+
+    public DuplicateUserException(String message) {
+        super(message);
+    }
+
+    public DuplicateUserException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
