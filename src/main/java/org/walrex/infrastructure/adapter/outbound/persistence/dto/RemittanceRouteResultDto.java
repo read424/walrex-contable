3b47a78a70c3dc5c8bfc8689e18948858cefa434
Ronald E. Aybar D <read424@gmail.com>
2026-02-11 05:55:01 -0500

    feat(infrastructure): add RemittanceRouteResultDto
    Created a persistence DTO for mapping native query results of remittance routes.

---
+++ src/main/java/org/walrex/infrastructure/adapter/outbound/persistence/dto/RemittanceRouteResultDto.java
@@ -0,0 +1,37 @@
+package org.walrex.infrastructure.adapter.outbound.persistence.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+public class RemittanceRouteResultDto {
+    private String codeIsoFrom;
+    private String nameIsoFrom;
+    private String symbolIsoFrom;
+    private String codeIsoTo;
+    private String nameIsoTo;
+    private String symbolIsoTo;
+    private String countryIso2;
+    private String countryIso3;
+    private String countryName;
+    private String countryFlag;
+    
+    // Constructor para usar con createNativeQuery
+    public RemittanceRouteResultDto(String codeIsoFrom, String nameIsoFrom, String symbolIsoFrom,
+                                   String codeIsoTo, String nameIsoTo, String symbolIsoTo,
+                                   String countryIso2, String countryIso3, String countryName, String countryFlag) {
+        this.codeIsoFrom = codeIsoFrom;
+        this.nameIsoFrom = nameIsoFrom;
+        this.symbolIsoFrom = symbolIsoFrom;
+        this.codeIsoTo = codeIsoTo;
+        this.nameIsoTo = nameIsoTo;
+        this.symbolIsoTo = symbolIsoTo;
+        this.countryIso2 = countryIso2;
+        this.countryIso3 = countryIso3;
+        this.countryName = countryName;
+        this.countryFlag = countryFlag;
+    }
+}
