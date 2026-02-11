e8bbbf119d0eda8190c02f4af51d1daa82cd9c18
Ronald E. Aybar D <read424@gmail.com>
2026-02-11 05:48:32 -0500

    feat(application): add CurrencyInfoResponse DTO
    Created a new DTO for currency info, including code, name, and symbol fields.

---
+++ src/main/java/org/walrex/application/dto/response/CurrencyInfoResponse.java
@@ -0,0 +1,18 @@
+package org.walrex.application.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrencyInfoResponse {
+    private String code;
+    private String name;
+    private String symbol;
+    //private List<ExchangeRateInfoResponse> rates;
+}
