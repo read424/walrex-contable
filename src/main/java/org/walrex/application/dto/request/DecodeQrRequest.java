93c2c33ff985edd54181548fcb4b7e28a0dac919
Ronald E. Aybar D <read424@gmail.com>
2026-02-12 13:12:20 -0500

    feat(emv-qr): add DecodeQrRequest DTO

---
+++ src/main/java/org/walrex/application/dto/request/DecodeQrRequest.java
@@ -0,0 +1,14 @@
+package org.walrex.application.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DecodeQrRequest {
+    private String qrCodeText;
+}
