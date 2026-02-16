a4565eadc8d5eeb0c7a704611fcbd0bc85c850c1
Ronald E. Aybar D <read424@gmail.com>
2026-02-16 16:51:10 -0500

    refactor(mapper): update BeneficiaryAccountDtoMapper
    Función: Mapeador que transforma el modelo de dominio BeneficiaryAccount a su representación DTO de respuesta.
    Cambios: Se ajustaron los mapeos para considerar la nueva jerarquía de objetos y el cambio a tipos Long en los identificadores.

--- src/main/java/org/walrex/infrastructure/adapter/inbound/mapper/BeneficiaryAccountDtoMapper.java
+++ src/main/java/org/walrex/infrastructure/adapter/inbound/mapper/BeneficiaryAccountDtoMapper.java
@@ -11,14 +11,7 @@ import java.util.List;
 @Mapper(componentModel = "jakarta", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface BeneficiaryAccountDtoMapper {
 
-    @Mapping(source = "customer.id", target = "customerId")
-    @Mapping(source = "customer.firstName", target = "customerName") // Assuming Customer has a firstName field
-    @Mapping(source = "bank.id", target = "bankId")
-    @Mapping(source = "bank.detName", target = "bankName")
-    @Mapping(source = "typeAccount.id", target = "typeAccountId")
-    @Mapping(source = "typeAccount.name", target = "typeAccountName")
-    @Mapping(source = "typeOperation.id", target = "typeOperationId")
-    @Mapping(source = "typeOperation.name", target = "typeOperationName")
+    @Mapping(source = "beneficiary.id", target = "beneficiaryId")
     BeneficiaryAccountResponse toResponse(BeneficiaryAccount beneficiaryAccount);
 
     List<BeneficiaryAccountResponse> toResponseList(List<BeneficiaryAccount> beneficiaryAccounts);
