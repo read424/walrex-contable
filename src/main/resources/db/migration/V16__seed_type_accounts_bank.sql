1109801da3fa4429ae2853ad3dfffe51e7f0230c
Ronald E. Aybar D <read424@gmail.com>
2026-05-26 06:22:12 -0500

    feat(db): agregar seed inicial de type_accounts_bank en V16

---
+++ src/main/resources/db/migration/V16__seed_type_accounts_bank.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.type_accounts_bank (id,det_name,status,created_at,updated_at) VALUES
+     (1,'Cta Cte','1','2026-01-13 06:13:01.287086-05','2026-01-13 06:13:01.287086-05'),
+     (2,'Cta Aho','1','2026-01-13 06:13:01.287086-05','2026-01-13 06:13:01.287086-05');
