6cbc7228ecd68b85af74732d650e08b695fe46c3
Ronald E. Aybar D <read424@gmail.com>
2026-05-26 06:22:12 -0500

    feat(db): agregar seed inicial de sunat_document_types en V12

---
+++ src/main/resources/db/migration/V12__seed_sunat_document_types.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.sunat_document_types (code,name,description,length,pattern,active,id,created_at,updated_at) VALUES
+       ('4','C.E.','Carnet de Extranjeria',NULL,NULL,true,5,'2025-12-15 09:45:24.502532-05','2025-12-15 09:45:24.511448-05'),
+       ('6','RUC','Registro Unico de Contribuyentes',NULL,NULL,true,6,'2025-12-15 09:45:24.502532-05','2025-12-15 09:45:24.511448-05'),
+       ('7','Pass','Pasaporte',NULL,NULL,true,7,'2025-12-15 09:45:24.502532-05','2025-12-15 09:45:24.511448-05'),
+       ('A','C.D.I.','Cedula Diplomatica de identidad',NULL,NULL,true,8,'2025-12-15 09:45:24.502532-05','2025-12-15 09:45:24.511448-05'),
+       ('B','D.I.P.R.N.D.','Documento identidad pais residencia-no.d',NULL,NULL,true,9,'2025-12-15 09:45:24.502532-05','2025-12-15 09:45:24.511448-05'),
+       ('C','TIN','Tax Identificacion Number - TIN - Doc Trib PP.NN',NULL,NULL,true,10,'2025-12-15 09:45:24.502532-05','2025-12-15 09:45:24.511448-05'),
+       ('D','I.N.','Identification Number - IN - Doc Trib PP.JJ',NULL,NULL,true,11,'2025-12-15 09:45:24.502532-05','2025-12-15 09:45:24.511448-05'),
+       ('E','TAM','TAM-Tarjeta Andina de Migracion',NULL,NULL,true,12,'2025-12-15 09:45:24.502532-05','2025-12-15 09:45:24.511448-05'),
+       ('F','PTP','Permiso Temporal de Permanencia PTP',NULL,NULL,true,13,'2025-12-15 09:45:24.502532-05','2025-12-15 09:45:24.511448-05'),
+       ('1','D.N.I.','Doc. Nacional de Identidad',9,'^[0-9]{9}$',true,2,'2025-12-15 09:45:24.502532-05','2025-12-15 12:02:14.952391-05'),
+       ('0','Doc.trib.no.dom.sin.ruc','Doc.trib.no.dom.sin.ruc',NULL,NULL,false,1,'2025-12-15 09:45:24.502532-05','2025-12-15 12:42:56.853961-05');
